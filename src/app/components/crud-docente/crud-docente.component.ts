2c98ac60dafd11b1e8ad10bdb0ffd1574755602f
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-06-01 21:30:04 -0500

    ajustes para incial

--- src/app/components/crud-docente/crud-docente.component.ts
+++ src/app/components/crud-docente/crud-docente.component.ts
@@ -37,87 +37,30 @@ export class CrudDocenteComponent implements OnInit {
   };
 
   constructor(private docenteService:DocenteService, private ubigeoService:UbigeoService) {
-      this.ubigeoService.listarDepartamento().subscribe(
-          response => this.departamentos = response
-      );            
-  }
+    this.ubigeoService.listarDepartamento().subscribe(
+        response => this.departamentos = response
+    );            
+}
 
   cargaProvincia(){
-      this.ubigeoService.listaProvincias(this.docente.ubigeo?.departamento).subscribe(
-        response =>  this.provincias= response
-      );
+    this.ubigeoService.listaProvincias(this.docente.ubigeo?.departamento).subscribe(
+      response =>  this.provincias= response
+    );
 
-      this.distritos = [];
-      this.docente.ubigeo!.idUbigeo = -1;
-      this.docente.ubigeo!.provincia = "-1";
-  }
+    this.distritos = [];
+    this.docente.ubigeo!.idUbigeo = -1;
+    this.docente.ubigeo!.provincia = "-1";
+}
 
-  cargaDistrito(){
-          this.ubigeoService.listaDistritos(this.docente.ubigeo?.departamento, this.docente.ubigeo?.provincia).subscribe(
-            response =>  this.distritos= response
-          );
-          
-          this.docente.ubigeo!.idUbigeo = -1;
-   }
-
-  ngOnInit(): void {
-  }
-
-  consulta(){
-        this.docenteService.consultaPorNombre(this.filtro==""?"todos":this.filtro).subscribe(
-              x => this.docentes = x
+cargaDistrito(){
+        this.ubigeoService.listaDistritos(this.docente.ubigeo?.departamento, this.docente.ubigeo?.provincia).subscribe(
+          response =>  this.distritos= response
         );
-  }
+        
+        this.docente.ubigeo!.idUbigeo = -1;
+ }
 
-  registra(){
-        this.docenteService.inserta(this.docente).subscribe(
-             x => Swal.fire('Mensaje', x.mensaje,'info') 
-        );
-  }
-
-  actualizaEstado(obj:Docente){
-      obj.estado = obj.estado == 1? 0 : 1;  
-      this.docenteService.actualiza(obj).subscribe();
-  }
-
-  busca(obj:Docente){
-      this.docente = obj;
-
-      this.ubigeoService.listaProvincias(this.docente.ubigeo?.departamento).subscribe(
-        response =>  this.provincias= response
-      );
-
-      this.ubigeoService.listaDistritos(this.docente.ubigeo?.departamento, this.docente.ubigeo?.provincia).subscribe(
-        response =>  this.distritos= response
-      );
-  }
-
-  actualiza(){
-      this.docenteService.actualiza(this.docente).subscribe(
-            x => Swal.fire('Mensaje', x.mensaje,'info') 
-      );
-  }
-
-
-  elimina(obj:Docente){
-          Swal.fire({
-            title: '¿Desea eliminar?',
-            text: "Los cambios no se van a revertir",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, elimina.',
-            cancelButtonText: 'No, cancelar'
-          }).then((result) => {
-              if (result.isConfirmed) {
-                
-                this.docenteService.elimina(obj.idDocente || 0).subscribe(
-                    x  =>   Swal.fire('Mensaje',x.mensaje,'success')
-                );
-                
-              }
-          })
-  }
+ ngOnInit(): void {
+}
 
 }
